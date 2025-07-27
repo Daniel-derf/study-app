46804344446dd72798a770d68b98361633c1701d
Daniel-derf <danielpardinho321@gmail.com>
2025-07-26 22:31:50 -0300

    docs: better auth module swagger summaries

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,7 +10,7 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @ApiOperation({ summary: 'Route to login' })
+  @ApiOperation({ summary: 'Log In with email and password' })
   @Post('login')
   async login(@Body() dto: LoginDto) {
     const jwt = await this.authService.login(dto);
@@ -18,7 +18,7 @@ export class AuthController {
     return { data: { jwt } };
   }
 
-  @ApiOperation({ summary: 'Route to recover password through email' })
+  @ApiOperation({ summary: 'Recover password with email' })
   @Post('forgot-password')
   async forgotPassword(@Body() dto: ForgotPasswordDto) {
     await this.authService.forgotPassoword(dto);
@@ -26,7 +26,7 @@ export class AuthController {
     return { message: 'A recovery email was sent to your email' };
   }
 
-  @ApiOperation({ summary: 'Route to confirm registered email' })
+  @ApiOperation({ summary: 'Confirm registered email passing the code' })
   @Post('confirm-email')
   async confirmEmail(@Body() dto: ConfirmEmailDto) {
     await this.authService.confirmEmail(dto);
