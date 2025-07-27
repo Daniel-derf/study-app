b0b43fb0c97c9e22179791703727ade684fe560f
Daniel-derf <danielpardinho321@gmail.com>
2025-07-26 22:35:53 -0300

    feat: jwt auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SubjectModule } from './subject/subject.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { GuardModule } from './guard/guard.module';
 
 @Module({
-  imports: [SubjectModule, AuthModule, UserModule, GuardModule],
-  controllers: [AppController],
+  imports: [SubjectModule, AuthModule, UserModule],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
