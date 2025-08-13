438f87ed157b040b2415080fc054d565fca28b1a
Daniel-derf <danielpardinho321@gmail.com>
2025-08-12 23:27:18 -0300

    feat: add finish study session use case

--- src/study-session/use-cases/finish-study-session.usecase.ts
+++ src/study-session/use-cases/finish-study-session.usecase.ts
@@ -1,6 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { IStudySessionRepository } from '../repository/study-session.interface.repository';
+import { StudySession } from '../entities/study-session.entity';
 
 @Injectable()
 export class FinishStudySessionUseCase {
-  constructor(private readonly sessionRepository) {}
+  constructor(
+    private readonly sessionRepository: IStudySessionRepository,
+    private readonly logger: Logger,
+  ) {}
+
+  async execute(input: Input) {
+    const duration = Math.floor(
+      (input.endDate.getTime() - input.startDate.getTime()) / 1000,
+    );
+
+    const session = StudySession.create({ ...input, duration });
+
+    await this.sessionRepository.save(session);
+
+    const durationMinutes = duration / 60;
+
+    this.logger.log(`Session of ${durationMinutes} minutes saved successfully`);
+  }
 }
+
+type Input = {
+  startDate: Date;
+  endDate: Date;
+  subjectId: string;
+  userId: string;
+};
